179c2cf9c6495480d677500bd838c9f9e5a37df3
an1mus <1stallways@gmail.com>
2020-01-15 20:00:35 +0200

    Added store

---
+++ src/redux/types/index.tsx
@@ -0,0 +1,3 @@
+import TODO_ACTIONS from './todo';
+
+export {TODO_ACTIONS};
---
+++ src/redux/types/todo.tsx
@@ -0,0 +1,7 @@
+const TODO_ACTIONS = {
+    ADD: 'ADD',
+    REMOVE: 'REMOVE',
+    UPDATE: 'UPDATE'
+};
+
+export default TODO_ACTIONS;
