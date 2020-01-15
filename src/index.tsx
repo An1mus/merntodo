345e340c477294e6075f8c5140b7347514c8600f
an1mus <1stallways@gmail.com>
2020-01-15 17:50:39 +0200

    Added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
