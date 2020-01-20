d5aed36f16a65c8064c9c041f2305b94a168bcf6
an1mus <1stallways@gmail.com>
2020-01-20 22:48:46 +0200

    Types update and description addition

--- src/components/Todo/List/index.tsx
+++ src/components/Todo/List/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ListItem from '../ListItem';
+import { TodoItem } from '../../../commons/types/todoItem';
 
-const List = ({onDelete, todos = []}: any,) => {
+interface Props {
+    todos: TodoItem[],
+    onDelete: (name: string) => void,
+}
+
+const List = ({onDelete, todos = []}: Props,) => {
+    const DESCRIPTION_TO_CHANGE = 'Some description'; //TODO: add description
     return (
         <>
             {
                 todos.length !== 0
-                    ? todos.map((item: any, index: number) => <ListItem key={index} name={item} onDelete={onDelete}/>)
+                    ? todos.map((item: any, index: number) => (
+                            <ListItem
+                                key={index}
+                                name={item}
+                                description={DESCRIPTION_TO_CHANGE}
+                                onDelete={onDelete}
+                            />
+                        ))
                     : <p>Nothing to do yet...</p>
             }
         </>
