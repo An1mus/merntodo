f9c2f2f716297fdd1f7a7b239d8f355484a8ee7f
an1mus <1stallways@gmail.com>
2020-01-29 14:11:33 +0200

    Nav and Header added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import Header from './common/Header/';
+import Nav from './common/Nav/'
 import Todo from './Todo';
 
 const App: React.FC = () => {
     return (
-        <div>
-            <Todo />
-        </div>
+        <HashRouter>
+            <div className={'u-flex u-flex-row'}>
+                <Nav/>
+                <div className={'u-flex u-flex-column'}>
+                    <Header />
+                    <Switch>
+                        <Route exact path={'/'} component={Todo} />
+                    </Switch>
+                </div>
+            </div>
+        </HashRouter>
     );
 };
 
---
+++ src/components/common/Nav/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <>
+            <NavLink to={'/'}>List</NavLink>
+        </>
+    );
+};
+
+export default Nav;
