6740d66f776ab447f617ed66c8171651d495848a
an1mus <1stallways@gmail.com>
2020-01-31 22:42:58 +0200

    Navigation links

--- src/components/common/Nav/index.tsx
+++ src/components/common/Nav/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import NAV_LINKS from './nav.config';
+import styled from 'styled-components';
+
+const NavContainer = styled.div`
+
+`;
 
 const Nav: React.FC = () => {
     return (
-        <>
+        <NavContainer className={'u-flex u-flex-column'}>
             {NAV_LINKS.map(
                 (link) => (<NavLink key={link.id} to={link.to}>{link.title}</NavLink>)
             )}
-        </>
+        </NavContainer>
     );
 };
 
