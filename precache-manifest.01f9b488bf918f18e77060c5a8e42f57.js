491a4bb92dbd8fca9471869c055ca55e3d082f86
an1mus <1stallways@gmail.com>
2020-02-15 13:10:18 +0200

    Updates

---
+++ precache-manifest.01f9b488bf918f18e77060c5a8e42f57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d935fd5407d2f7a157825879ddc46fc",
+    "url": "/merntodo/index.html"
+  },
+  {
+    "revision": "f8d0f59db6b1e1106549",
+    "url": "/merntodo/static/css/main.bba1ddcb.chunk.css"
+  },
+  {
+    "revision": "d43e5aaec9df786b1800",
+    "url": "/merntodo/static/js/2.a56e27da.chunk.js"
+  },
+  {
+    "revision": "c3d6a8ffe504ff94840dd784346d078f",
+    "url": "/merntodo/static/js/2.a56e27da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8d0f59db6b1e1106549",
+    "url": "/merntodo/static/js/main.be6ae89e.chunk.js"
+  },
+  {
+    "revision": "d4bb4bd344c969001c84",
+    "url": "/merntodo/static/js/runtime-main.b9e16512.js"
+  },
+  {
+    "revision": "98384d0b5900eabdff811991b6c816e7",
+    "url": "/merntodo/static/media/FilterIcon.98384d0b.svg"
+  }
+]);
