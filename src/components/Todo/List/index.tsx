0c14d87aa1d9aeabc984cdb00cea329eeb5a7fe9
an1mus <1stallways@gmail.com>
2020-02-16 10:46:58 +0200

    Added item id as key

--- src/components/Todo/List/index.tsx
+++ src/components/Todo/List/index.tsx
@@ -16,7 +16,7 @@ const List = ({onDelete, updateTodo, todos = []}: Props,) => {
                     ? todos.map((item: any, index: number) => (
                             <ListItem
                                 key={index}
-                                item={item}
+                                item={item.id}
                                 onDelete={onDelete}
                                 updateTodo={updateTodo}
                             />
