f45ce333ecffac3041b2b230c65386de07b1343a
an1mus <1stallways@gmail.com>
2020-02-22 16:27:28 +0200

    Icons animations

--- src/components/common/Nav/NavItem.tsx
+++ src/components/common/Nav/NavItem.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavItemContainer = styled.div`
     align-items: center;
     cursor: pointer;
     display: flex;
-    margin-bottom: 0.5rem;
     
     svg {
-        margin-right: 0.5rem
+        margin-right: 1rem
+    }
+    
+    .iconStroke,
+    .iconFill {
+        transition: all .3s;
+    }
+    
+    .iconStroke {
+        stroke: var(--main-idle-icon-color);
+    }
+    .iconFill {
+        fill: var(--main-idle-icon-color);
+    }
+    
+    &:hover .iconStroke,
+    &.active .iconStroke {
+        stroke: var(--main-highlight-color);
+        transition: all .3s;
+    }
+    
+    &:hover .iconFill,
+    &.active .iconFill {
+        fill: var(--main-highlight-color);
+        transition: all .3s;
     }
 `;
 
