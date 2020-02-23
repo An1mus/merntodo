fc047b9f103288f57704f1f3c29f893ba92c78b5
an1mus <1stallways@gmail.com>
2020-02-23 13:00:44 +0200

    Navigation styling and logo placeholder

--- src/components/common/Nav/index.tsx
+++ src/components/common/Nav/index.tsx
@@ -3,23 +3,41 @@ import NAV_LINKS from './nav.config';
 import styled from 'styled-components';
 import NavItem from './NavItem';
 
+const LogoContainer = styled.div`
+    margin: 2rem auto 6rem 2.15rem;
+    display: flex;
+    width: 100%;    
+    
+    p {
+        border: 1px solid red; 
+    }
+`;
+
 const NavContainer = styled.div`
+    bottom: 0;
     background: white;
+    display: flex;
+    left: 0;
     position: fixed;
     top: 0;
-    left: 0;
-    bottom: 0;
-    width: 10rem;
+    width: 14rem;
 `;
+
 
 const Nav: React.FC = () => {
     return (
         <NavContainer className={'u-flex u-flex-column'}>
-            {NAV_LINKS.map(
-                ({id, to, title, Icon}) => (
-                    <NavItem key={id} to={to} title={title} Icon={Icon}/>
-                )
-            )}
+            <LogoContainer>
+                <p>Todo logo placeholder</p>
+            </LogoContainer>
+
+            <div>
+                {NAV_LINKS.map(
+                    ({id, to, title, Icon}) => (
+                        <NavItem key={id} to={to} title={title} Icon={Icon}/>
+                    )
+                )}
+            </div>
         </NavContainer>
     );
 };
