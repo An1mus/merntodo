6843230a832639bfbca440fbf5657be60372f36f
An1mus <an1mus@icloud.com>
2023-02-09 21:10:35 +0200

    Removing headless:true from playwright config'

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
                 video: 'on-first-retry',
                 colorScheme: 'dark',
                 launchOptions: {
-                    headless: false,
+                    headless: true,
                     slowMo: 1500,
                 },
                 ...devices['Desktop Chrome'],
