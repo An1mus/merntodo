1bfa3a4c1b771d7a4e15ebf95af0fd1b3b12532c
An1mus <an1mus@icloud.com>
2023-02-09 19:24:02 +0200

    Spacing

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -8,11 +8,26 @@ interface Props {
     onChange: React.EventHandler<any>;
     isError?: boolean;
     id?: string;
+    onKeyDown?: React.EventHandler<any>
 }
 
-const Input: React.FC<Props> = ({type, value, placeholder, onChange, isError= false, id}) => {
-    return <input id={id} className={`${styles.input} ${isError ? styles.error : ''}`} type={type} value={value} onChange={onChange}
-                  placeholder={placeholder}/>;
+const Input: React.FC<Props> = ({
+                                    type,
+                                    value,
+                                    placeholder,
+                                    onChange,
+                                    isError = false,
+                                    id,
+                                    onKeyDown
+    }) => {
+    return <input
+        id={id} className={`${styles.input} ${isError ? styles.error : ''}`}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        onKeyDown={onKeyDown}
+    />;
 }
 
 export default Input;
--- src/routes/ToDoList/AddToDoItem/index.tsx
+++ src/routes/ToDoList/AddToDoItem/index.tsx
@@ -28,7 +28,6 @@ const AddToDoItem: React.FC = () => {
             return;
         }
 
-
         await addItemToTheList(newTodoItemName, newTodoItemDescription, duration);
 
         setNewTodoItemName('');
